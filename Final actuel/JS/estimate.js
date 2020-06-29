05baa47d94aa4dd86c315e58a6115ece0eaa6e3d
Hugo-Azix <60874858+Hugo-Azix@users.noreply.github.com>
2020-06-29 14:08:33 +0200

    Suppression btn login

--- Final actuel/JS/estimate.js
+++ Final actuel/JS/estimate.js
@@ -135,35 +135,4 @@ function fillPopup(){
 
 
 }
-var btnlogout = document.querySelector("#logout");
-var btnlogin = document.querySelector("#login");
-var btnregister = document.querySelector("#register");
-NamePerson = localStorage.getItem("NamePerson");
-btnlogout.style.display = "none";
 
-btnregister.addEventListener("click", function () {
-    localStorage.setItem("NamePerson", "");
-    window.location.href = "../HTML/register.html";
-});
-
-btnlogin.addEventListener("click", function () {
-  NamePerson = localStorage.setItem("NamePerson", "");
-    window.location.href = "../HTML/Login.html";
-});
-
-btnlogout.addEventListener("click", function () {
-   NamePerson = localStorage.setItem("NamePerson", "");
-   window.location.href = "../HTML/Accueil.html";
-});
-
-if(NamePerson !== ""){
-    btnlogin.style.display = "none";
-    btnregister.style.display = "none";
-    document.querySelector("#name").innerHTML = NamePerson;
-    btnlogout.style.display = "inline-block";
-    console.log(NamePerson);
-}
-else{
-    btnlogin.style.display = "inline-block";
-    btnregister.style.display = "inline-block";
-}
